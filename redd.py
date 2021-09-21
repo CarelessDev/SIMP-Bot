1617923e0f36cd26226ac3e5198a1a3493e3e88a
Leomotors (Ubuntu) <59821765+Leomotors@users.noreply.github.com>
2021-09-21 11:02:02 +0700

    Improve Performance

--- redd.py
+++ redd.py
@@ -1,6 +1,12 @@
 from libraryimport import *
-reddit = asyncpraw.Reddit(client_id = "spAS-xBqUiRD8SV8A6D1Gg", 
-                    client_secret="vJJzxCPen2sjXAdX_kTPZxMMCnf5hQ",
-                    username="Informal-Ad-2398",
-                    password="Qwentyrufuwojd1",
-                    user_agent="kawaiipraw")
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+reddit = asyncpraw.Reddit(client_id=os.getenv("client_id"),
+                          client_secret=os.getenv("client_secret"),
+                          username=os.getenv("username"),
+                          password=os.getenv("password"),
+                          user_agent=os.getenv("user_agent"))
